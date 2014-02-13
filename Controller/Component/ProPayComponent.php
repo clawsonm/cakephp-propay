2cf2fde80116bc470faee46a65d0ec8f4176d567
Michael Clawson <macaronisoft@gmail.com>
2014-02-13 09:42:12 -0700

    #5: create payer WIP

--- Controller/Component/ProPayComponent.php
+++ Controller/Component/ProPayComponent.php
@@ -10,8 +10,27 @@
  */
 
 App::uses('Component', 'Controller');
+App::uses('CakeEventManager', 'Event');
 
+/**
+ * Class ProPayComponent
+ *
+ * @category Controller/Component
+ * @package  cakephp-propay
+ * @author   Michael Clawson <macaronisoft@gmail.com>
+ * @license  MIT License
+ * @link     https://github.com/clawsonm/cakephp-propay
+ */
 class ProPayComponent extends Component {
+
+	protected $_SPS;
+
+	protected $_ID;
+
+	public function initialize($controller) {
+		$this->_SPS = new SPS();
+		$this->_ID = new ID(Configure::read('ProPay.AuthenticationToken'), Configure::read('ProPay.BillerAccountId'));
+	}
 
 /**
  * preauth the given card with the given data, and given amount
@@ -21,7 +40,38 @@ class ProPayComponent extends Component {
  * @return array
  */
 	public function preAuthCard($data) {
-		return array('success' => true, 'auth_code' => 'test');
+		$status = $this->preAuthCard($data);
+
+		return $status;
+	}
+
+/**
+ * call the soap createPayer routine, and return data via events
+ *
+ * @param $payerData
+ *
+ * @return boolean
+ */
+	public function createPayer($payerData) {
+		$createPayer = new CreatePayer($this->_ID, $payerData['id']);
+		$createPayerResponse = $this->_SPS->CreatePayer($createPayer);
+
+		if ($createPayerResponse->CreatePayerResult->RequestResult->ResultCode == '00') {
+			$payerId = $createPayerResponse->CreatePayerResult->ExternalAccountID;
+			$event = new CakeEvent(
+				'ProPay.Component.PrePay.createdPayer',
+				$this,
+				array(
+					'id' => $payerData['id'],
+					'payerId' => $payerId
+				)
+			);
+			CakeEventManager::instance()->dispatch($event);
+			return true;
+		} else {
+			debug($createPayerResponse);
+			return false;
+		}
 	}
 
 } 
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 cakephp-propay
 ==============
 
-wrapper for propay SOAP service in cakephp. Source WSDL is located at [http://protectpay.propay.com/API/SPS.svc?wsdl](http://protectpay.propay.com/API/SPS.svc?wsdl).
+wrapper for propay SOAP service in cakephp. Source WSDL is located at [http://protectpay.propay.com/API/SPS.svc?wsdl](http://protectpay.propay.com/API/SPS.svc?wsdl). However to test your implementation you must use their test service located at [http://protectpaytest.propay.com/api/sps.svc?wsdl](http://protectpaytest.propay.com/api/sps.svc?wsdl)
 
 Installation
 =============
@@ -11,3 +11,5 @@ You must generate the SOAP client with wsdl2phpgenerator from [github.com/wsdl2p
 e.g. Run:
 
     /path/to/wsdl2phpgenerator-2.2.2.phar -i http://protectpay.propay.com/API/SPS.svc?wsdl -o generated/
+
+    /path/to/wsdl2phpgenerator-2.2.2.phar -i http://protectpaytest.propay.com/api/sps.svc?wsdl -o generated/
